a047e69e1544bf91aea7bbca6a328f9e76ea2a5d
Mikael Siidorow <mikael.siidorow@aalto.fi>
2023-11-11 10:26:32 +0200

    feat: experiment with heart rate meter

--- src/lib/components/camera.svelte
+++ src/lib/components/camera.svelte
@@ -1,11 +1,25 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
 
+	const SUPPORS_MEDIA_DEVICES = browser && 'mediaDevices' in navigator;
 	const SUPPORTS_IMAGE_CAPTURE = browser && 'ImageCapture' in window;
 
-	let videoSource: HTMLVideoElement | null = null;
-	let loading = false;
+	let videoElement: HTMLVideoElement | null = null;
+	let samplingCanvas: HTMLCanvasElement | null = null;
+	$: samplingContext = samplingCanvas?.getContext('2d', {
+		willReadFrequently: true
+	});
+	type Sample = {
+		value: number;
+		time: number;
+	};
 
+	let sampleBuffer: Sample[] = [];
+	const maxSamples = 60 * 5; // 5 minutes of samples
+
+	let bpm: number | undefined;
+
+	let loading = false;
 	const obtainVideoCamera = async () => {
 		try {
 			loading = true;
@@ -22,7 +36,11 @@
 					deviceId: camera.deviceId,
 					facingMode: 'environment',
 					width: { ideal: 4096 },
-					height: { ideal: 2160 }
+					height: { ideal: 2160 },
+
+					whiteBalanceMode: 'manual',
+					exposureMode: 'manual',
+					focusMode: 'manual'
 				}
 			});
 			const videoTrack = mediaStream.getVideoTracks()[0];
@@ -34,9 +52,13 @@
 				});
 			}
 
-			if (videoSource) {
-				videoSource.srcObject = mediaStream;
-				videoSource.play();
+			if (videoElement) {
+				videoElement.srcObject = mediaStream;
+				videoElement.play();
+
+				setTimeout(() => {
+					monitorLoop();
+				}, 1000);
 			} else {
 				console.error('videoSource not found');
 			}
@@ -45,13 +67,128 @@
 			console.error(error);
 		}
 	};
+
+	const monitorLoop = () => {
+		processFrame();
+		window.requestAnimationFrame(monitorLoop);
+	};
+
+	const averageBrightness = (canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) => {
+		// 1d array of r, g, b, a pixel data values
+		const pixelData = context.getImageData(0, 0, canvas.width, canvas.height).data;
+		let sum = 0;
+
+		// Only use the red and green channels as that combination gives the best readings
+		for (let i = 0; i < pixelData.length; i += 4) {
+			sum = sum + pixelData[i] + pixelData[i + 1];
+		}
+
+		// Since we only process two channels out of four we scale the data length to half
+		const avg = sum / (pixelData.length * 0.5);
+
+		// Scale to 0 ... 1
+		return avg / 255;
+	};
+
+	const getAverageCrossings = (samples: Sample[], average: number) => {
+		// Get each sample at points where the graph has crossed below the average level
+		// These are visible as the rising edges that pass the midpoint of the graph
+		const crossingsSamples: Sample[] = [];
+		let previousSample = samples[0]; // Avoid if statement in loop
+
+		samples.forEach(function (currentSample) {
+			// Check if next sample has gone below average.
+			if (currentSample.value < average && previousSample.value > average) {
+				crossingsSamples.push(currentSample);
+			}
+
+			previousSample = currentSample;
+		});
+
+		return crossingsSamples;
+	};
+
+	const analyzeData = (samples: Sample[]) => {
+		// Get the mean average value of the samples
+		const average = samples.map((sample) => sample.value).reduce((a, c) => a + c) / samples.length;
+
+		// Find the lowest and highest sample values in the data
+		// Used for both calculating bpm and fitting the graph in the canvas
+		let min = samples[0].value;
+		let max = samples[0].value;
+		samples.forEach((sample) => {
+			if (sample.value > max) {
+				max = sample.value;
+			}
+			if (sample.value < min) {
+				min = sample.value;
+			}
+		});
+
+		// The range of the change in values
+		// For a good measurement it should be between  ~ 0.002 - 0.02
+		const range = max - min;
+
+		const crossings = getAverageCrossings(samples, average);
+		return {
+			average,
+			min,
+			max,
+			range,
+			crossings
+		};
+	};
+
+	const calculateBpm = (samples: Sample[]) => {
+		if (samples.length < 2) {
+			return;
+		}
+
+		const averageInterval =
+			(samples[samples.length - 1].time - samples[0].time) / (samples.length - 1);
+
+		return 60000 / averageInterval;
+	};
+
+	const processFrame = () => {
+		// Get a sample from the canvas pixels
+		if (!samplingCanvas || !samplingContext || !videoElement) {
+			throw new Error('Sampling canvas, context or video not found');
+		}
+		samplingContext.drawImage(videoElement, 0, 0, samplingCanvas.width, samplingCanvas.height);
+
+		const value = averageBrightness(samplingCanvas, samplingContext);
+		const time = Date.now();
+
+		sampleBuffer.push({ value, time });
+		if (sampleBuffer.length > maxSamples) {
+			sampleBuffer.shift();
+		}
+
+		const dataStats = analyzeData(sampleBuffer);
+
+		// TODO: Store BPM values in array and display moving average
+		const calculatedBpm = calculateBpm(dataStats.crossings);
+		bpm = calculatedBpm;
+	};
 </script>
 
 <div>
+	{#if !SUPPORS_MEDIA_DEVICES}
+		<p>Media devices not supported</p>
+	{/if}
+	{#if !SUPPORTS_IMAGE_CAPTURE}
+		<p>Image capture not supported</p>
+	{/if}
+
 	{#if loading}
 		<p>Loading...</p>
 	{/if}
-	<!-- svelte-ignore a11y-media-has-caption -->
-	<video bind:this={videoSource} />
+	<p>BPM: {bpm?.toLocaleString('en', { maximumSignificantDigits: 2 }) ?? '-'}</p>
+	<div class="sr-only">
+		<!-- svelte-ignore a11y-media-has-caption -->
+		<video id="camera-feed" bind:this={videoElement} />
+		<canvas id="sampling-canvas" width="30" height="30" bind:this={samplingCanvas} />
+	</div>
 	<button on:click={obtainVideoCamera}>Get camera</button>
 </div>
