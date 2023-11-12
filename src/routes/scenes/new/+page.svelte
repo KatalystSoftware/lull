4c194897914648901b6d76a8498beed234864fe9
patrr2 <ezhacks2014@gmail.com>
2023-11-12 02:31:58 +0200

    fix buttons

--- src/routes/scenes/new/+page.svelte
+++ src/routes/scenes/new/+page.svelte
@@ -1,14 +1,19 @@
+<style>
+	li a {
+		display: block;
+	}
+</style>
 <main class="space-y-12">
 	<h2 class="text-2xl font-medium text-center">How are you feeling?</h2>
 	<ul class="text-center font-medium text-lg space-y-4">
-		<li class="bg-white/40 backdrop-blur-lg rounded-lg px-8 py-6">
-			<a href="/camera/scene">Stressed (live)</a>
+		<li class="bg-white/40 backdrop-blur-lg rounded-lg">
+			<a href="/camera/scene" class="px-8 py-6">Stressed (live)</a>
 		</li>
-		<li class="bg-white/40 backdrop-blur-lg rounded-lg px-8 py-6">
-			<a href="/camera/scene?demo=true&debug=true">Anxious (demo)</a>
+		<li class="bg-white/40 backdrop-blur-lg rounded-lg">
+			<a href="/camera/scene?demo=true&debug=true" class="px-8 py-6">Anxious (demo)</a>
 		</li>
-		<li class="bg-white/40 backdrop-blur-lg rounded-lg px-8 py-6">
-			<a href="/camera/scene?debug=true">Headache (live + debug)</a>
+		<li class="bg-white/40 backdrop-blur-lg rounded-lg">
+			<a href="/camera/scene?debug=true" class="px-8 py-6">Headache (live + debug)</a>
 		</li>
 	</ul>
 </main>
